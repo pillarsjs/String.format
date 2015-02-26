b6ab572fc0a184e5b3583e1e7699336013a934a8
Bifuer <bifuer@odeux.com>
2015-02-26 09:00:47 +0100

    fix for (undefined | number) results

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ function format(string,params){
     try {
       capture = '["'+capture.split('.').join('"]["')+'"]';
       replace = eval('params'+capture);
-      replace = JSON.decycled(replace).replace(/(^\"|\"$)/g,'').replace(/(\\n|\\r)/g,'\n').replace(/\\t/g,'\t');
+      if(typeof replace === 'string'){
+        replace = JSON.decycled(replace).replace(/(^\"|\"$)/g,'').replace(/(\\n|\\r)/g,'\n').replace(/\\t/g,'\t');
+      }
       return replace;
     } catch(error){
       return match;
