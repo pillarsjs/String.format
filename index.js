fd5e781519bc6034065ef7d7eddab86d9e6c20fe
Bifuer <bifuer@odeux.com>
2015-04-08 11:22:01 +0200

    new pattern support ·{}· for eval code

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ String.prototype.format = function(params){
   return format(this.toString(),params);
 };
 function format(string,params){
-  var replaced = string.replace(/\{([a-z0-9\$\_]+(?:\.[a-z0-9\-\_]+)*)\}/gmi,function(match,capture,index,all){
+  var replaced = string;
+  replaced = replaced.replace(/\·\{(.*)\}\·/gmi,function(match,capture,index,all){
+    var replace;
+    try {
+      replace = eval(capture);
+      replace = JSON.decycled(replace);
+      if(typeof replace === 'string'){
+        replace = replace.replace(/(^\"|\"$)/g,'').replace(/(\\n|\\r)/g,'\n').replace(/\\t/g,'\t');
+      }
+      return replace;
+    } catch(error){
+      return match;
+    }
+  });
+  replaced = replaced.replace(/\{([a-z0-9\$\_]+(?:\.[a-z0-9\-\_]+)*)\}/gmi,function(match,capture,index,all){
     var replace;
     try {
       capture = '["'+capture.split('.').join('"]["')+'"]';
